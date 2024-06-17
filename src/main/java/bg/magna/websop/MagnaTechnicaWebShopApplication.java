8f31312fc7430fe5f9bbff7ad6fac5fc1398911e
ChristopherHartarski <c.hartarski@gmail.com>
2024-06-17 13:46:26 +0300

    Initial commit (created Spring project)

---
+++ src/main/java/bg/magna/websop/MagnaTechnicaWebShopApplication.java
@@ -0,0 +1,13 @@
+package bg.magna.websop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagnaTechnicaWebShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagnaTechnicaWebShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bg/magna/websop/MagnaTechnicaWebShopApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.magna.websop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagnaTechnicaWebShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
