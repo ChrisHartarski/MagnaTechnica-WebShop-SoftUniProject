953def7ae1156809ddf99b05c4c4dae193bacbb1
ChristopherHartarski <c.hartarski@gmail.com>
2024-06-18 12:36:25 +0300

    Added templates for index, contacts, login, register. Added fragments for navbar and footer. Added controllers for with getMappings for said templates.

---
+++ src/main/java/bg/magna/websop/controller/HomeController.java
@@ -0,0 +1,18 @@
+package bg.magna.websop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String viewIndex() {
+        return "index";
+    }
+
+    @GetMapping("/about")
+    public String viewAbout() {
+        return "about";
+    }
+
+}
---
+++ src/main/java/bg/magna/websop/controller/RegisterController.java
@@ -0,0 +1,13 @@
+package bg.magna.websop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RegisterController {
+
+    @GetMapping("/register")
+    public String viewRegister() {
+        return "register";
+    }
+}
