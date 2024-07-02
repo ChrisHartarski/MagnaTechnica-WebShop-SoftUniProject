f6d5c640613267581d2cdd1f6abcd1a43bae63c6
ChristopherHartarski <c.hartarski@gmail.com>
2024-07-02 15:00:30 +0300

    Visual fixes on Webshop page

--- src/main/java/bg/magna/websop/service/impl/BrandServiceImpl.java
+++ src/main/java/bg/magna/websop/service/impl/BrandServiceImpl.java
@@ -5,9 +5,7 @@ import bg.magna.websop.repository.BrandRepository;
 import bg.magna.websop.service.BrandService;
 import org.springframework.stereotype.Service;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
