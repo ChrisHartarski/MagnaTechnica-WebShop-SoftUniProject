fc4331959e03b41227d1820c06b1beaa6aa50c63
ChristopherHartarski <c.hartarski@gmail.com>
2024-07-10 13:50:09 +0300

    Moved the web-shop and contacts GetMapping to Home controller. Removed ContactController.java and WebShopController.java.

--- src/main/java/bg/magna/websop/controller/HomeController.java
+++ src/main/java/bg/magna/websop/controller/HomeController.java
@@ -1,16 +1,39 @@
 package bg.magna.websop.controller;
 
+import bg.magna.websop.model.dto.ShortPartDataDTO;
 import bg.magna.websop.service.BrandService;
 import bg.magna.websop.service.PartService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
+    private PartService partService;
+
+    public HomeController(PartService partService) {
+        this.partService = partService;
+    }
 
     @GetMapping("/")
     public String viewIndex() {
         return "index";
+    }
+
+    @GetMapping("/web-shop")
+    public String viewWebShop(Model model) {
+
+        List<ShortPartDataDTO> parts = partService.getAllShortPartDTOs();
+        model.addAttribute("parts", parts);
+
+        return "web-shop";
+    }
+
+    @GetMapping("/contact")
+    public String viewContact() {
+        return "contact";
     }
 
 }
