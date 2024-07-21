265cc94821629145e2d1b44757cc0a108c8ae609
ChristopherHartarski <c.hartarski@gmail.com>
2024-07-21 11:50:26 +0300

    Added activeLink to navbar. Changed admin-panel buttons. Added View all orders and view all enquiries buttons to admin actions. Removed Orders button from navbar. TODO: view all enquiries in admin-panel

--- src/main/java/bg/magna/websop/config/SecurityConfig.java
+++ src/main/java/bg/magna/websop/config/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
                                 //grant access only to admin users
                                 .requestMatchers("/admin-panel", "/machines/add", "/machines/delete/", "/machines/edit/", "/brands/add", "/admin-panel/initializeMockDB", "/orders/all", "/orders/dispatch/", "/orders/deliver/", "/parts/add", "/parts/edit/", "/parts/delete/", "/machines/enquiries/all").hasRole("ADMIN")
                                 //grant access only to normal users
-                                .requestMatchers("/cart", "/cart/remove-item/", "/orders/add", "machines/enquiries/").hasRole("USER")
+                                .requestMatchers("/cart", "/cart/remove-item/", "/orders/add", "machines/enquiries/all").hasRole("USER")
                                 .anyRequest()
                                 .authenticated()
                 )
