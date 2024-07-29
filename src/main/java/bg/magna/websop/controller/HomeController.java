d656d1879a4bac4b9cd273a7982bb2b69088b501
ChristopherHartarski <c.hartarski@gmail.com>
2024-07-29 13:23:23 +0300

    Added google maps to contact template.

--- src/main/java/bg/magna/websop/controller/HomeController.java
+++ src/main/java/bg/magna/websop/controller/HomeController.java
@@ -1,6 +1,7 @@
 package bg.magna.websop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -12,7 +13,8 @@ public class HomeController {
     }
 
     @GetMapping("/contact")
-    public String viewContact() {
+    public String viewContact(Model model) {
+        model.addAttribute("mapsApiKey", System.getenv("MAPS_API_KEY"));
         return "contact";
     }
 
